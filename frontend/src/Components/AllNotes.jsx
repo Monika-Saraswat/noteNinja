2b7949b252d73dc34090b53cbc4ae65ceea972b4
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-07-20 14:01:37 +0530

    UI

--- frontend/src/Components/AllNotes.jsx
+++ frontend/src/Components/AllNotes.jsx
@@ -3,7 +3,23 @@ import Note from './Note'
 
 const AllNotes = () => {
   return (
-    <div className='grid gap-1 overflow-auto grid-flow-col'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-4 overflow-auto'>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
         <Note/>
         <Note/>
         <Note/>
