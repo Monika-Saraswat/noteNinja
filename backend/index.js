97493eb9b6af563c0ec47b6e0b97f4a7d7bbbf72
Muskangupta2507 <100120446+Muskangupta2507@users.noreply.github.com>
2024-07-20 14:05:12 +0530

    xyz

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use('/api/auth',authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb+srv://saraswatmonika55:noteninja@cluster0.dvivdh4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://saraswatmonika55:noteninja@cluster0.dvivdh4.mongodb.net/notNinja?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
     console.log("DB connected");
 })
