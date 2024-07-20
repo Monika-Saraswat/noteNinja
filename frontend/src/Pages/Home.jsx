4780518d04a7ca8458076d5563533645b19685f0
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-07-20 13:32:35 +0530

    UI

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import LogOutBtn from '../Components/LogOutBtn.jsx'
+import AllNotes from '../Components/AllNotes.jsx'
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 shadow-md'>
+    <div className='flex h-screen w-screen rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 shadow-md align-middle items-center'>
+      <AllNotes/>
       <LogOutBtn/>
     </div>
   )
